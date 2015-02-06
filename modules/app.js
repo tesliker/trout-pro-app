14c6b9e7f798fcfa00a73d05efcaa6dabcfe2f30
root <root@d1stkfactory>
2015-02-06 04:10:50 +0000

    adding streams detail

--- modules/app.js
+++ modules/app.js
@@ -5,9 +5,8 @@
     });
     
   app.controller('StoreController', function($scope, $http){
+    $scope.streamDetail = 0;
     $scope.location = {};
-    $scope.location.latitude = "44.444";
-    $scope.location.longitude = "-73.6311421";
     $scope.location.miles = "1000";
     
     $scope.location.submitForm = function() {
@@ -20,9 +19,10 @@
       }
       function showPosition(position) {
       $scope.location.latitude = position.coords.latitude;
-      console.log(position.coords.latitude);
+      console.log($scope.location.latitude);
       $scope.location.longitude = position.coords.longitude;
-      $http.get('http://www.troutprostore.com/streams-services?distance%5Blatitude%5D=' + $scope.location.latitude +'&distance%5Blongitude%5D=' + '-73.6311421' + '&distance%5Bsearch_distance%5D=' + $scope.location.miles + '&distance%5Bsearch_units%5D=mile').
+      console.log($scope.location.longitude);
+      $http.get('http://www.troutprostore.com/streams-services?distance%5Blatitude%5D=' + $scope.location.latitude +'&distance%5Blongitude%5D=' + $scope.location.longitude + '&distance%5Bsearch_distance%5D=' + $scope.location.miles + '&distance%5Bsearch_units%5D=mile').
       
       success(function(data, status, headers, config){
         $scope.nodes = data.nodes
@@ -32,6 +32,22 @@
       getLocation();
       console.log('hello');
       
+    }
+    
+    $scope.getNode = function(nid) {
+      console.log(nid);
+      $scope.nid = nid;
+      $scope.streamDetail = 1;
+      $http.get('http://www.troutprostore.com/node/' + nid +'.json').
+      
+      success(function(data, status, headers, config){
+        $scope.detailNode = data
+        console.log('YES!');
+      });
+    }
+    
+    $scope.backToStart = function() {
+      $scope.streamDetail = 0;
     }
   });
   
