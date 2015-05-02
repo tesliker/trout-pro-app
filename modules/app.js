411b53639b9e3d1286c739ca3e9ee82c2905602c
root <root@d1stkfactory>
2015-05-02 05:29:10 +0000

    A couple changes

--- modules/app.js
+++ modules/app.js
@@ -1,55 +1,69 @@
 (function(){
-  var app = angular.module('store', []);
-  app.config(function ($httpProvider) {
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  var troutPro = angular.module('troutPro', ['ngRoute']);
+
+    troutPro.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
     });
-    
-  app.controller('StoreController', function($scope, $http){
-    $scope.streamDetail = 0;
-    $scope.location = {};
-    $scope.location.miles = "1000";
-    
-    $scope.location.submitForm = function() {
-      function getLocation() {
-          if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(showPosition);
-          } else {
-              x.innerHTML = "Geolocation is not supported by this browser.";
-          }
+
+    // create the controller and inject Angular's $scope
+    troutPro.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+        setInterval(function() {
+        $('.carousel').carousel('next');
+        }, 5000);
+    });
+
+    troutPro.controller('aboutController', function($scope, $http) {
+      $scope.location = [];
+      $scope.location.miles = "1000";
+      $scope.location.submitForm = function() {
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+        function showPosition(position) {
+        $scope.location.latitude = position.coords.latitude;
+        console.log($scope.location.latitude);
+        $scope.location.longitude = position.coords.longitude;
+        console.log($scope.location.longitude);
+        $http.get('http://www.troutprostore.com/streams-services?distance%5Blatitude%5D=' + $scope.location.latitude +'&distance%5Blongitude%5D=' + $scope.location.longitude + '&distance%5Bsearch_distance%5D=' + $scope.location.miles + '&distance%5Bsearch_units%5D=mile').
+        
+        success(function(data, status, headers, config){
+          $scope.nodes = data.nodes
+          console.log('YES!');
+        }); 
+        }
+        getLocation();
+        console.log('hello');
+        
       }
-      function showPosition(position) {
-      $scope.location.latitude = position.coords.latitude;
-      console.log($scope.location.latitude);
-      $scope.location.longitude = position.coords.longitude;
-      console.log($scope.location.longitude);
-      $http.get('http://www.troutprostore.com/streams-services?distance%5Blatitude%5D=' + $scope.location.latitude +'&distance%5Blongitude%5D=' + $scope.location.longitude + '&distance%5Bsearch_distance%5D=' + $scope.location.miles + '&distance%5Bsearch_units%5D=mile').
-      
-      success(function(data, status, headers, config){
-        $scope.nodes = data.nodes
-        console.log('YES!');
-      }); 
-      }
-      getLocation();
-      console.log('hello');
-      
-    }
-    
-    $scope.getNode = function(nid) {
-      console.log(nid);
-      $scope.nid = nid;
-      $scope.streamDetail = 1;
-      $http.get('http://www.troutprostore.com/node/' + nid +'.json').
-      
-      success(function(data, status, headers, config){
-        $scope.detailNode = data
-        console.log('YES!');
-      });
-    }
-    
-    $scope.backToStart = function() {
-      $scope.streamDetail = 0;
-    }
-  });
-  
+      $scope.location.submitForm();
+    });
+
+    troutPro.controller('contactController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
   
 })();
